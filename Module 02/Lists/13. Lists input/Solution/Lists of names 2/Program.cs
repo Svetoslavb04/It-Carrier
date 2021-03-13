d4786121a17de10c4a5441967bb8b522cf302122
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2021-03-13 12:46:58 +0200

    Solved

---
+++ Module 02/Lists/13. Lists input/Solution/Lists of names 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lists_of_names_2
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            List<string> fullNames = Console.ReadLine().Split(',').ToList();
+
+            foreach (var fullName in fullNames)
+            {
+                List<string> names = fullName.Split().ToList();
+                names.Reverse();
+
+                foreach (var name in names)
+                {
+                    Console.Write($"{name} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Module 02/Lists/13. Lists input/Solution/Lists of names/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lists_of_names
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            List<string> names = Console.ReadLine().Split().ToList();
+
+            Console.WriteLine(string.Join("; ", names));
+        }
+    }
+}
