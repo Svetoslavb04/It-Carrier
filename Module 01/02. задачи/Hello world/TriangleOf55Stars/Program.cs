8fe3cef44ac43e0efea7ad007edcd66d295f9389
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2021-03-13 12:23:38 +0200

    First commit
    Preps

---
+++ Module 01/02. задачи/Hello world/TriangleOf55Stars/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TriangleOf55Stars
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int i = 1; i < 11; i++)
+            {
+                for (int r = 0; r < i; r++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Module 01/02. задачи/Hello world/kvadratOtZvezdi/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace kvadratOtZvezdi
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < n-2; i++)
+            {
+                Console.WriteLine($"*{new string(' ', n - 2)}*");
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
