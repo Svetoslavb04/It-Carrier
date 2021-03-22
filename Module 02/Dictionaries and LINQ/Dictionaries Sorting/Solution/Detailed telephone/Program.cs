8cad14f312c6374f80a122f587f7531eab10b306
unknown <39881430+Svetoslavb04@users.noreply.github.com>
2021-03-22 18:21:46 +0200

    sorteddics tasks

---
+++ Module 02/Dictionaries and LINQ/Dictionaries Sorting/Solution/Detailed telephone/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Detailed_telephone
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var telephone = new SortedDictionary<string, string>();
+
+            while (true)
+            {
+                var input = Console.ReadLine().Split(' ').ToList<string>();
+                string command = input[0];
+
+                if (command == "END")
+                {
+                    break;
+                }
+                if (command == "ListAll")
+                {
+                    foreach (var contact in telephone)
+                    {
+                        Console.WriteLine($"{contact.Key} -> {contact.Value}");
+                    }
+                }
+                if (command == "A")
+                {
+                    string name = input[1];
+                    string number = (input[2]);
+
+                    if (telephone.ContainsKey(name))
+                    {
+                        telephone[name] = number;
+                    }
+                    else
+                    {
+                        telephone.Add(name, number);
+                    }
+                }
+                if (command == "S")
+                {
+                    string name = input[1];
+
+                    if (telephone.ContainsKey(name))
+                    {
+                        Console.WriteLine($"{name} -> {telephone[name]}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Contact {name} does not exist.");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Module 02/Dictionaries and LINQ/Dictionaries Sorting/Solution/Real numbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Real_numbers
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var sortedDic = new SortedDictionary<double, int>();
+
+            var line = Console.ReadLine().Split().ToArray();
+
+            foreach (var item in line)
+            {
+                if (sortedDic.ContainsKey(double.Parse(item)))
+                {
+                    sortedDic[double.Parse(item)]++;
+                }
+                else
+                {
+                    sortedDic.Add(double.Parse(item), 1);
+                }
+            }
+
+            foreach (var item in sortedDic)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
